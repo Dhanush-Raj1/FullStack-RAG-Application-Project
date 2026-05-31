633ca73588feed387eac5a2bab020ee4d47c8b37
Dhanush <dhanush1004raj@gmail.com>
2026-05-31 23:34:51 +0530

    feat(models): add schema for retriver result

--- src/models/document.py
+++ src/models/document.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class DocumentMetadata:
     source: str
@@ -18,4 +19,19 @@ class DocumentMetadata:
 class Document:
     page_content: str
     metadata: DocumentMetadata
-    
+
+
+@dataclass
+class DocumentRetrievalResult:
+    id: str
+    text: str
+    score: float
+
+    source: str
+    file_type: str
+
+    page: Optional[int]
+    section: Optional[str]
+
+    chunk_index: Optional[int]
+    parent_document_id: Optional[str]
