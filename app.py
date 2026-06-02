4b48ced999cf0c497983d0ab45c21b239115d2c4
Dhanush <dhanush1004raj@gmail.com>
2026-06-02 10:51:38 +0530

    fix(app): update to get all kind of API requests

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "https://rag-frontend-b75n.onrender.com/"],  # Vite's default local port and deployed frontend URL
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
